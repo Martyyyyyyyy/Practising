2799c4110345bff133794c52940a65a05d14f508
Martin Fesenko <Mfesenko2003@gmail.com>
2022-09-09 12:16:34 +0300

    js practise

--- practising.js
+++ practising.js
@@ -94,7 +94,7 @@ const pastaIndex = groceryList.indexOf('pasta');
 
 console.log(pastaIndex);
 
-//nested for loops
+//Nested for loops
 const bobsFollowers = ['Martin', 'Max', 'John', 'Cambell'];
 const tinasFollowers = ['Martin', 'Max', 'Judith'];
 const mutualFollowers = [];
@@ -105,14 +105,16 @@ for (let i = 0; i < bobsFollowers.length; i++) {
     }
   }
 }
-// do while loop
+
+// Do while loop
 let cupsOfSugarNeeded = 23;
 let cupsAdded = 0;
 do {
  cupsAdded++
  console.log(cupsAdded + ' cup was added') 
 } while (cupsAdded < cupsOfSugarNeeded);
-// break keyword
+
+// Break keyword
 for (let i = 0; i < rapperArray.length; i++){
     console.log(rapperArray[i]);
     if (rapperArray[i] === 'Notorious B.I.G.'){
@@ -121,7 +123,8 @@ for (let i = 0; i < rapperArray.length; i++){
   }
   
   console.log("And if you don't know, now you know.");
-//higher-order functions 
+
+// Higher-order functions 
 const addTwo = num => {
   return num + 2;
 }
@@ -137,7 +140,8 @@ const checkConsistentOutput = (func, val) => {
 }
   
 console.log(checkConsistentOutput(addTwo, 5));
-//Iterators
+
+// Iterators
 const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
 
 artists.forEach(artist => {
@@ -174,26 +178,143 @@ const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
 console.log(words.some((word) => {
   return word.length < 6;
 }));
-//more iterators
+
+// More iterators
 const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
 
 const nums = [1, 50, 75, 200, 350, 525, 1000];
 
-//  Choose a method that will return undefined
+//Choose a method that will return undefined
 cities.forEach(city => console.log('Have you visited ' + city + '?'));
 
-// Choose a method that will return a new array
+//Choose a method that will return a new array
 const longCities = cities.filter(city => city.length > 7);
 
-// Choose a method that will return a single value
+//Choose a method that will return a single value
 const word = cities.reduce((acc, currVal) => {
   return acc + currVal[0]
 }, "C");
 
 console.log(word)
 
-// Choose a method that will return a new array
+//Choose a method that will return a new array
 const smallerNums = nums.map(num => num - 5);
 
-// Choose a method that will return a boolean value
+//Choose a method that will return a boolean value
 nums.every(num => num < 0);
+
+// Objects
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+let variable = spaceship.numCrew;
+let variable1 = spaceship.flightPath;
+
+// Bracket notation
+let isActive = spaceship['Active Mission'];
+console.log(isActive);
+
+// Methods
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+// Write your code below
+let alienShip = {
+  retreat() {
+    console.log(retreatMessage)
+  },
+  takeOff() {
+    console.log('Spim... Borp... Glix... Blastoff!')
+  }
+};
+
+alienShip.retreat();
+alienShip.takeOff();
+
+let spaceship2000 = {
+  passengers: [{name: 'Space Dog'}],
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032 
+  },
+  crew: {
+    captain: { 
+      name: 'Sandra', 
+      degree: 'Computer Engineering', 
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    'back-up': {
+      battery: "Lithium",
+      terabytes: 50
+    }
+  }
+}; 
+
+let capFave = spaceship2000.crew.captain['favorite foods'][0];
+let firstPassenger = spaceship2000.passengers[0];
+
+// Pass by reference
+let spaceship3000 = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
+};
+
+// Write your code below
+let greenEnergy = objectParam => {
+  objectParam['Fuel Type'] = 'avocado oil';
+};
+
+let remotelyDisable = objectParam => {
+  objectParam.disabled  = true;
+};
+
+greenEnergy(spaceship3000);
+remotelyDisable(spaceship3000);
+console.log(spaceship3000);
+
+// Looping through objects
+let spaceship4000 = {
+  crew: {
+  captain: { 
+      name: 'Lily', 
+      degree: 'Computer Engineering', 
+      cheerTeam() { console.log('You got this!') } 
+      },
+  'chief officer': { 
+      name: 'Dan', 
+      degree: 'Aerospace Engineering', 
+      agree() { console.log('I agree, captain!') } 
+      },
+  medic: { 
+      name: 'Clementine', 
+      degree: 'Physics', 
+      announce() { console.log(`Jets on!`) } },
+  translator: {
+      name: 'Shauna', 
+      degree: 'Conservation Science', 
+      powerFuel() { console.log('The tank is full!') } 
+      }
+  }
+};
+
+// for...in
+for (let crewMember in spaceship.crew) {
+  console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`)
+};
+for (let crewMember in spaceship.crew) {
+  console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`)
+};
